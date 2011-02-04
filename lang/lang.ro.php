bc98018ed6e0da10b5220906e90e7e85934941ac
Vlad <vlad_micutul@yahoo.com>
2011-02-04 22:19:16 +0800

    Added romanian translation

---
+++ lang/lang.ro.php
@@ -0,0 +1,46 @@
+<?php
+
+	$about = array(
+		'name' => 'Română',
+		'author' => array(
+			'name' => 'Vlad Ghiţă',
+			'email' => 'vlad_micutul@yahoo.com',
+		),
+		'release-date' => '2011-02-04'
+	);
+
+	/**
+	 * Localisation Manager
+	 */
+	$dictionary = array(
+		'Localisations' => 
+		'Localizări',
+
+		'Sort output naturally' => 
+		'Sortare naturală a rezultatului',
+
+		'Localisation Manager' => 
+		'Administratorul de Localizări',
+
+		'Language Manager' => 
+		'Administratorul de Limbi',
+
+		'Available dictionaries' => 
+		'Dicţionare disponibile',
+
+		'Action' => 
+		'Acţiune',
+
+		'Create new dictionary' => 
+		'Adaugă un dicţionar',
+
+		'Your content here' => 
+		'Conţinutul tău aici',
+
+		'TEXT' => 
+		'TEXT',
+
+		'Symphony\'s backend' => 
+		'Administrarea pentru Symphony',
+
+	);
